074fad8a922d141db1e6756a22c6a9d6d7e31039
Dan Williams <me@deedubs.com>
2012-03-25 09:24:16 -0400

    add async test

--- test.js
+++ test.js
@@ -6,8 +6,15 @@ describe('My Example', function() {
   });
 
   it('should fail if it expects anything but greatness', function() {
-    expect(isGreat).to.equal(false);
+    expect(isGreat).to.equal(true);
   });
+
+	it('should be an async test', function(done) {
+	  setTimeout(function() {
+		  expect(done).to.be.a(Function); // Calling done lets mocha know the test exec'd
+		  done();
+		},50);
+	});
 
   it('should have a pending test');
 });
